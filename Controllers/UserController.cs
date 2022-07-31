3da16272512539a2a5faff371da81751e3deccfd
Akif-Imran <raiden3322@gmail.com>
2022-07-31 19:24:38 +0500

    Delted WeatherForecast added UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttendanceApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UserController : ControllerBase
+  {
+  }
+}
